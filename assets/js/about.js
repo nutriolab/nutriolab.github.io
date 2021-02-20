9c87fcc468c0130bcab49e17636fb48bcb563452
Kelvien Hidayat <kelvienhidayat@gmail.com>
2021-02-20 00:20:29 -0500

    Checkpoint

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,16 +1,32 @@
-const setupAbout = () => {
-  const topPart = document.getElementsByClassName('top')[0];
-  const aboutUsTitle = document.getElementsByClassName('about-us-title')[0];
-  const topWidth = topPart.clientWidth + topPart.offsetParent.offsetLeft;
-  const topHeight = aboutUsTitle.clientHeight + topPart.offsetParent.offsetTop;
-  const lineEndTop = document.getElementsByClassName('line-end-top')[0];
-  const lineEndTopSvg = lineEndTop.getElementsByTagName('svg')[0];
-  lineEndTop.style.width = `${topWidth * 2}px`;
-  const padding = 25;
-  lineEndTop.style.top = `-${lineEndTopSvg.clientHeight - topHeight - padding}px`;
-  lineEndTop.style.left = `-${topPart.clientWidth}px`;
-
-  const lineEndBottom = document.getElementsByClassName('line-end-bottom')[0];
+const setup = () => {
+  setupHorizontalSquare();
+  setupVerticalSquare();
 }
 
-window.onload = setupAbout
+const padding = 25;
+const setupHorizontalSquare = () => {
+  const horizontalSquare = document.getElementsByClassName('rect-horizontal')[0];
+  const horizontalSquareSvg = horizontalSquare.getElementsByTagName('svg')[0];
+  
+  const aboutUsTitle = document.getElementsByClassName('about-us-title')[0];
+  const width = aboutUsTitle.clientWidth + aboutUsTitle.offsetParent.offsetLeft;
+  horizontalSquare.style.width = `${width * 2}px`;
+
+  const offsetTop = horizontalSquareSvg.clientHeight - (aboutUsTitle.offsetParent.offsetTop + aboutUsTitle.clientHeight + padding);
+  horizontalSquare.style.top = `-${offsetTop}px`;
+  horizontalSquare.style.left = `-${width}px`;
+};
+
+const setupVerticalSquare = () => {
+  const verticalSquare = document.getElementsByClassName('rect-vertical')[0];
+
+  const visMis = document.getElementsByClassName('vision-mission')[0];
+  const ctaContainer = document.getElementsByClassName('cta-container')[0];
+  const values = document.getElementsByClassName('values')[0];
+  const footerContainer = document.getElementsByClassName('footer-container')[0];
+  const rightPadding = window.innerWidth - visMis.getBoundingClientRect().right;
+  const width = visMis.clientWidth + rightPadding;
+
+  verticalSquare.style.width = `${width * 2}px`;
+  verticalSquare.style.left = `${visMis.offsetParent.offsetLeft}px`;
+};
--- assets/js/team.js
+++ assets/js/team.js
@@ -1,3 +1,10 @@
+const setupLine = () => {
+  const line = document.getElementsByClassName('line')[0];
+  line.style.height = `${document.body.clientHeight}px`;
+  const teamSection = document.getElementsByClassName('team-section')[0];
+  line.style.left = `${teamSection.getBoundingClientRect().right}px`;
+};
+
 const setup = () => {
-  
+  setupLine();  
 }
