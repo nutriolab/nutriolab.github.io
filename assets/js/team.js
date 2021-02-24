fc73de36fa5ac398ec08b213a1e1617ff75dc598
Kelvien Hidayat <kelvienhidayat@gmail.com>
2021-02-23 23:23:28 -0500

    New line in team

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,8 +1,8 @@
 const setupLine = () => {
-  const line = document.getElementsByClassName('line')[0];
+  const line = document.getElementsByClassName('line-connector')[0];
   line.style.height = `${document.body.clientHeight}px`;
   const teamSection = document.getElementsByClassName('team-section')[0];
-  line.style.left = `${teamSection.getBoundingClientRect().right}px`;
+  line.style.left = `${teamSection.getBoundingClientRect().right - line.clientWidth}px`;
 };
 
 const setup = () => {
